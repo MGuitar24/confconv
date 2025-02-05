c8cf179f063d3ca190ca6ed60c6fc215283930e6
mguitar24 <mguitar24@gmail.com>
2025-02-04 17:37:45 -0700

    adding func RewriteJSONToYAML

--- yaml.go
+++ yaml.go
@@ -2,11 +2,13 @@ package confconv
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
 
-func ConvertJSONToYML(data []byte) ([]byte, error) {
+func ConvertJSONToYAML(data []byte) ([]byte, error) {
 	var jsonData map[string]interface{}
 	err := json.Unmarshal(data, &jsonData)
 	if err != nil {
@@ -14,4 +16,42 @@ func ConvertJSONToYML(data []byte) ([]byte, error) {
 	}
 	ymlData, err := yaml.Marshal(jsonData)
 	return ymlData, err
+}
+
+func RewriteJSONToYAML(path string) error {
+	if fileExists(path) {
+		fileData, err := readFile(path)
+		if err != nil {
+			return fmt.Errorf("failed to read file %s: %w", path, err)
+		}
+
+		jsonData, err := ConvertJSONToYAML(fileData)
+		if err != nil {
+			return fmt.Errorf("failed to convert file to JSON %s: %w", path, err)
+		}
+
+		fileExt := identifyFileType(path)
+		switch fileExt {
+		case "json":
+			{
+				jsonPath := strings.Replace(path, ".json", ".yaml", 1)
+				err = writeFile(jsonPath, jsonData)
+				if err != nil {
+					return fmt.Errorf("failed to write YAML file %s: %w", path, err)
+				}
+				return nil
+			}
+		default:
+			{
+				jsonPath := path + ".yaml"
+				err = writeFile(jsonPath, jsonData)
+				if err != nil {
+					return fmt.Errorf("failed to write YAML file %s: %w", path, err)
+				}
+				return nil
+			}
+		}
+	} else {
+		return fmt.Errorf("file %s does not exist", path)
+	}
 }
